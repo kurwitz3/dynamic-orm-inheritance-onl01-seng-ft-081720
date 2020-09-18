8dad2bc3986fb8fc515fbe38422756e6bc2b82f9
Richard Kurwitz <kurwitz3@aol.com>
2020-09-18 19:10:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require_relative "./interactive_record.rb"
 class Song < InteractiveRecord
 
   self.column_names.each do |col|
-    attr_accessor col_name.to_sym
+    attr_accessor col.to_sym
   end
 
 end
